b4308d92ec60f84d01365c533793bf8dc4d99d0e
Sanjay S <sanjays12042000@gmail.com>
2025-11-07 19:37:31 +0530

    added

---
+++ js/day1.js
@@ -0,0 +1,66 @@
+// console.log("revamp")
+
+// var a=5;
+// let b=7;
+// const c=8;
+// console.log(++a);
+
+// var a = 5;
+// var b = 6;
+
+// console.log(`Addition of two numbers ${a+b}`);
+// console.log(`subtraction of two numbers ${a-b}`);
+// console.log(`Multification of two numbers ${a*b}`);
+// console.log(`Devision of two numbers ${a/b}`);
+// console.log(`module of two numbers ${a%b}`);      
+
+// find type
+
+// var a = 12;
+// console.log(typeof a);
+// var b="12";
+// console.log(typeof b);
+
+
+// function add(){
+//     let age =18;
+//     if(age>=18){
+//         console.log(`vote is eligible ${age}`);
+//     } 
+//     else{
+//         console.log(`not eligible ${age}`);
+//     }
+// }
+// add();
+
+// function add(a){
+//      if(a>=18){
+//         console.log(`vote is eligible ${a}`);
+//     } 
+//     else{
+//         console.log(`not eligible ${a}`);
+//     }
+// }
+// add(17);
+
+// ex
+
+// var factor="ajith";
+// var fmovie="veeram";
+// var fplayer="thala";
+// function favourite(){
+//     console.log(factor);
+//     console.log(fplayer);
+//     console.log(fmovie);
+// }
+// favourite();
+
+//  function area(a,b) {
+//      console.log(a*b);
+    
+//  }
+//  area(2,4)
+// for (let i=0; i<=6; i++){
+//     console.log(i);
+    
+// }
---
+++ js/day2.js
@@ -0,0 +1,19 @@
+// var arr = [5,6,7,"sanjay"];
+
+// console.log(arr.length);
+
+// var arr = [5,6,7,"sanjay","ramya"];
+// arr.pop(1)
+// console.log(arr);
+
+var obj = {
+   Name:"Sanjay",
+   class:"BE",
+   Location: "NTM",
+   Fees:[4999,"offers"]
+}
+
+obj;
+
+console.log(obj);
+
